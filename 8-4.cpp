1bee642b89718886461c65e33a969feca716bf95
m-schwanekamp <112090940+m-schwanekamp@users.noreply.github.com>
2022-10-26 20:18:39 -0400

    Add files via upload

---
+++ 8-4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <iomanip>
+using namespace std;
+int romanCharValue(char r);
+//yields the numeric value of the passed Roman numeral character.
+int convertRomanToInt(string s);
+//this function converts the Roman number string into its 
+//equivalent integer value by implementing the following algorithm.
+
+int main () {
+    char r;
+    string s;
+
+    cout << "Enter a Roman number or Q to quit: ";
+    cin >> r;
+    if (r != 'Q') {
+        
+    }
+    else return 0;
+}
